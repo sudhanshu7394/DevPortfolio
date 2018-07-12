abb055007fb3d2f6fd717dd10d0779915f822636
sudhanshu7394 <sudhanshu.yadav31@gmail.com>
2018-07-12 14:48:12 +0530

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,12 +45,11 @@ module ApplicationHelper
     ]
   end  
 
-  def nav_helper style,tag_type
+  def nav_helper style, tag_type
     nav_links = ''
 
     nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>
-"
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
 
