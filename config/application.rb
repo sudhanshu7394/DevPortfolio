e4804edff1e57cfa6db6afcaf2ee739c47198d96
sudhanshu7394 <sudhanshu.yadav31@gmail.com>
2018-09-04 18:29:45 +0530

    updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,6 @@ Bundler.require(*Rails.groups)
 
 module DevPortfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
